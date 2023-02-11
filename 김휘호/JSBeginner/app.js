bda247e4c63c5f3282d3450674113b4588419095
Hwiho <rlagnlgh12@naver.com>
2023-02-11 15:21:13 +0900

    painting colorful lines with mouse

--- 김휘호/JSBeginner/app.js
+++ 김휘호/JSBeginner/app.js
@@ -3,16 +3,22 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200,200,15,100);
-ctx.fillRect(350,200,15,100);
-ctx.fillRect(252.5,200,60,200);
+ctx.lineWidth = 2;
 
+const colors = [
+    "#1abc9c",
+    "#3498db",
+    "#e74c3c",
+    "#2c3e50"
+];
 
-ctx.arc(282.5,150,50,0,2*Math.PI);
-ctx.fill();
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(400,400);
+    const color = colors[Math.floor(Math.random()*colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(265,125,10,0,1*Math.PI);
-ctx.arc(300,125,10,0,1*Math.PI);
-ctx.fill();
+canvas.addEventListener("mousemove",onClick);
