21216a325d17434b56a9a8b8489e433ba44a3c79
Hwiho <rlagnlgh12@naver.com>
2023-02-11 16:16:15 +0900

    real painting with mousedown

--- 김휘호/JSBeginner/app.js
+++ 김휘호/JSBeginner/app.js
@@ -2,23 +2,31 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
 
-const colors = [
-    "#1abc9c",
-    "#3498db",
-    "#e74c3c",
-    "#2c3e50"
-];
+let isPainting = false;
+/*변수선언*/
 
-function onClick(event) {
-    ctx.beginPath();
-    ctx.moveTo(400,400);
-    const color = colors[Math.floor(Math.random()*colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove",onClick);
+function onMouseDown(){
+    isPainting = true;
+}
+
+function CancelPainting(){
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", CancelPainting);
+canvas.addEventListener("mouseleave", CancelPainting);
+/*document.addEventListener("mouseleave", CancelPainting);
+document로 mouseleave 설정*/
