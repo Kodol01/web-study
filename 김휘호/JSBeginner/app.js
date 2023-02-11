aa7a12ae1ba70a9fa7604e1da79c480d28e5c847
Hwiho <rlagnlgh12@naver.com>
2023-02-11 19:15:39 +0900

    fill color in canvas

--- 김휘호/JSBeginner/app.js
+++ 김휘호/JSBeginner/app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOption = Array.from(document.getElementsByClassName("color-option"));
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -9,6 +10,7 @@ canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 /*변수선언*/
 
 function onMove(event){
@@ -45,12 +47,32 @@ function onColorClick(event){
     color.value = colorValue;
 }
 
+function onModeClick(){
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }
+    else{
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.beginPath();
+        ctx.fillRect(0,0,800,800);
+    }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", CancelPainting);
 canvas.addEventListener("mouseleave", CancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOption.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
